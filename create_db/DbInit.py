6fe3168a533493422f00f50eacfc8d91c3d706d8
mikemartinez13 <mcmartinez@wm.edu>
2023-11-05 14:41:04 -0500

    Added multiple company functionality, where multiple companies can be added at the same time. Tested and worked. Few bugs left to work on pertaining to deciding when to create the database object or not.

--- create_db/DbInit.py
+++ create_db/DbInit.py
@@ -1,90 +1,141 @@
 import pandas as pd
+import numpy as np
 import sqlite3
 import os
 import requests
 
 class ISDb:
     """
-    A class using FMP API to fetch company income statement data and process it into a database.
+    A class to fetch company income statement data and process it into a database. 
+    Initializes income statement database based on desired filepath. Also fetches raw income statement data. Powered by FMP API, sqlite, and pandas.
 
     Attributes:
         path_db (str): desired filepath for database object.
-        company_ticker (str): company ticker as listed on U.S. Stock Exchanges (e.g. Apple = 'AAPL').
+        company_tickers (list): list of companies to query data from. Formatted as listed on U.S. Stock Exchanges (e.g. Apple = 'AAPL').
         years (int): years of past data desired for the company.
-        create (bool, default to False): if True, create a new database if path_db does not exist.
+        replace (bool, default to False): if True, replace and overwrite database at filepath. If False.
     """
     def __init__(self,
             path_db: str,
-            company_ticker: str,
+            company_tickers: list,
             years: int,
-            create: bool = False
+            replace: bool = False
                 ): # Path to the database file # Should we create a new database if it doesn't exist?
-        """
-        Initializes an empty database based on desired filepath and fetches raw income statement data. 
-        Parameters:
-            path_db (str): desired filepath for database object.
-            company_ticker (str): company ticker as listed on U.S. Stock Exchanges (e.g. Apple = 'AAPL').
-            years (int): years of past data desired for the company.
-            create (bool, default to False): if True, create a new database if path_db does not exist.
-        """
-        # Check if the file does not exist
-        if not os.path.exists(path_db):
-            # Should we create it?
-            if create:
-                self.conn = sqlite3.connect(path_db)
-                self.conn.close()
-            else:
-                raise FileNotFoundError(path_db + ' does not exist. Perhaps your file path is incorrect?')
         
+        full_is_data = []
         API_KEY = input("Your FMP API key here:")
-        income_statement = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{company_ticker}?limit={years}&apikey={API_KEY}")
-
-        income_statement_json = income_statement.json()
+        #query company data individually
+        for comp in company_tickers:
+            income_statement = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{comp}?limit={years}&apikey={API_KEY}")
+            income_statement_json = income_statement.json()
+            full_is_data.extend(income_statement_json)
         
+        # Check if the file does not exist
+        #if not os.path.exists(path_db):
+            # Should we create it?
+            #if create:
+             #   self.conn = sqlite3.connect(path_db)
+            #else:
+             #   raise FileNotFoundError(path_db + ' does not exist. Perhaps your file path is incorrect?')
+        self.__conn = sqlite3.connect(path_db)
+
+        income_statement_df = pd.DataFrame(full_is_data)
+
+        if not replace:
+            raise Exception("Data already exists in the file! Write to a new filepath.")
+        else:
+            if os.path.isfile(path_db):
+                confirmation = input("WARNING! You are about to overwrite existing data in the table. Would you like to proceed? y/n: ")
+                if confirmation == 'y':
+                    income_statement_df.to_sql('i_s_table', self.__conn,index = False, if_exists = 'replace')
+                    print('Database successfully created!')
+                else: 
+                    raise Exception("That was a close one! Next time, write to a new filepath.")
+            else:
+                income_statement_df.to_sql('i_s_table', self.__conn,index = False, if_exists = 'replace')
+                print('Database successfully created!')
+
         self.__path_db = path_db
-        self.__json_data = income_statement_json
+        self.__json_data = full_is_data
         self.__years = years
-        self.__company = company_ticker
+        self.__companies = company_tickers
+        self.__df = income_statement_df
+
+        self.__conn.close()
 
         return
     # Intialize the income_statement request and get the raw data
 
     def get_rawdata(self):
+        '''
+        Returns raw income statement data as JSON.
+        '''
         return self.__json_data.copy()
     
-    def income_statement_df(self): # create database from FMP API income statement data
+    def get_df(self): # create database from FMP API income statement data
         '''
         Returns pandas DataFrame created from income statement JSON raw data.
         '''
-        df = pd.DataFrame(self.__json_data.copy())
-        return df
+        return self.__df.copy()
 
     def income_statement_csv(self):
         '''
-        Writes to a csv file created from income statement JSON raw data.
+        Writes to a csv file created from income statement JSON raw data. Returns none.
         '''
         df = self.income_statement_df()
         # write file_path check for overwriting csv files
         df.to_csv(f"Income_statement_{self.__company}.csv")
 
         return
+
+    '''def income_statement_db(self, if_exists = {'fail','replace','append'}): # fix this later
+        
+        Inserts income statement data into empty dataframe. Returns none.
+        Parameters:
+            if_exists {'fail','replace','append'}: Determines action to be completed if data exists in db. If 'fail', raise error. 
+            If 'replace', display warning and then overwrite existing data. If 'append', append data to the end of the table.
+        
+        df = self.income_statement_df()
+        if if_exists == 'fail':
+            raise Exception("Data already exists in the file! Write to a new filepath.")
+        elif if_exists == 'replace':
+            confirmation = input("WARNING! You are about to overwrite existing data in the table. Would you like to proceed? y/n: ")
+            if confirmation == 'y':
+                df.to_sql('i/s_table', self.connect(),index = False, if_exists = if_exists)
+            else: 
+                raise Exception("Data already exists in the file! Write to a new filepath.")
+        elif if_exists == 'append':
+            print("i/s data has been appended to existing table.")
+            df.to_sql('i/s_table')
+        
+        return'''
     
     def connect(self) -> None:
-        self.conn = sqlite3.connect(self.__path_db)
-        self.curs = self.conn.cursor()
+        '''
+        Opens connection (for reading) and cursor object (for writing) to database file. Returns none. 
+        '''
+        self.__conn = sqlite3.connect(self.__path_db)
+        self.__curs = self.__conn.cursor()
         return
     
     def close(self) -> None:
-        self.conn.close()
+        '''
+        Closes connection. Returns none.
+        '''
+        self.__conn.close()
         return
     
     def run_query(self, sql:str):
-        '''Use this method for running SELECT queries only'''
+        """
+        Use this method for running SELECT queries only. NO create queries.
+        Parameters:
+            sql (str): your sql SELECT code
+        """
         self.connect()
-        results = pd.read_sql(sql, self.conn)
+        results = pd.read_sql(sql, self.__conn)
         self.close()
         return results
 
     def __str__(self):
-        message = (f"The company in the data is {self.__company}, and the data spans the course of the last {self.__years} years.")
+        message = (f"The company in the data is {self.__companies}, and the data spans the course of the last {self.__years} years.")
         return message
--- create_db/testing.py
+++ create_db/testing.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
 import requests
+import sqlite3
 from DbInit import ISDb
 
-db = ISDb(path_db = 'isdb.db', company_ticker='AAPL', years = 5, create=True)
+db = ISDb(path_db = 'isdb.db', company_tickers=['AAPL','MSFT','TSLA','NVDA','META','GOOGL','AMZN'], years = 5, replace=True)
 
-db.income_statement_df()
 
-print(db)
+db.connect()
+sql = '''
+SELECT * 
+FROM i_s_table 
+;
+'''
+print(db.get_rawdata())
 
+print(db.get_df())
+
+print(db.run_query(sql)['symbol'].value_counts())
